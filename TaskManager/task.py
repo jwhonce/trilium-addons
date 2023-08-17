a3c633b2c43bc78d003d91e6b1be0cf891dfb802
Jhon Honce <jhonce@redhat.com>
2023-08-17 16:36:34 -0700

    Clean up task.py
    * Make autocorrection work
    * Clean up help output, aka rewrite docstrings
    
    Signed-off-by: Jhon Honce <jhonce@redhat.com>

--- TaskManager/task.py
+++ TaskManager/task.py
@@ -1,173 +1,162 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """Curate tasks for Task Manager in trilium notes.
 
-Note:
-    This is a work in progress and an exercise in learning typer and TriliumAlchemy
+Note(s):
+    * This is a work in progress and an exercise in learning typer and TriliumAlchemy
+    * The labels, relations and widgets of TaskManager are leveraged to manage tasks
 """
+import os
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Annotated, Optional
-import click
+from functools import cache
+from typing import Annotated, Generator, Optional
 
 import typer
 from rich.console import Console
 from rich.table import Table
 from trilium_alchemy import Label, Note, Session
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
-cli = typer.Typer(context_settings={"help_option_names": ["-h", "--help"]})
+cli = typer.Typer(
+    rich_markup_mode="markdown",
+    context_settings={"help_option_names": ["--help", "-h"], "allow_interspersed_args": True},
+)
 
 
 def version_callback(value: bool):
-    """Print version and exit"""
+    """Print version and exit."""
     if value:
-        typer.echo(f"tm version: {__version__}")
+        typer.echo(f"task v: {__version__}")
         raise typer.Exit()
 
 
-def complete_description(ctx: typer.Context, incomplete: str) -> list[str]:
+@cache
+def open_session(ctx: typer.Context) -> Session:
+    session = Session(os.environ["TRILIUM_URL"], os.environ["TRILIUM_TOKEN"])
+    ctx.with_resource(session)
+    return session
+
+
+def complete_description(ctx: typer.Context, incomplete: str) -> Generator[str, None, None]:
     """Helper for autocompletion of description.
 
     :param ctx: typer context
     :param incomplete: partial description
     :return: list of matching descriptions
     """
-    if incomplete == "" or incomplete is None:
-        return [t.title for t in ctx.obj.root.children]
-    return [t.title for t in ctx.obj.root.children if t.title.startswith(incomplete)]
+    session: Session = open_session(ctx)
+
+    # Build query string based on command
+    query = "#task"
+
+    if ctx.command.name not in ("archive", "delete", "rm"):
+        query += " #!doneDate"
+
+    if incomplete:
+        query += f' note.title =* "{incomplete}"'
+
+    for task in session.search(query):
+        yield task.title
 
 
-# Type alias for description
 Description = Annotated[
-    str, typer.Argument(help="Description of task item", autocompletion=complete_description)
+    str, typer.Argument(help="Description of Task.", autocompletion=complete_description)
 ]
-
-
-# https://typer.tiangolo.com/tutorial/parameter-types/custom-types/
-class TaskNote(Note, click.ParamType):
-    name = "Note"
-
-    def convert(self, value, param, ctx) -> Note:
-        return super().search(f'#task note.title="{value}"', ancestor_note=ctx.obj.root)[0]
 
 
 @dataclass(frozen=True)
 class State:
-    """Record state for tm application.
+    """Record state of application.
 
-    :param trilium: instance of Session
-    :param root: #taskTodoRoot note for TaskManager
     :param verbose: display additional columns or data
     :param dry_run: render table rather than updating Trilium
     """
 
-    trilium: Session
-    root: Note
-    verbose: bool = False
-    dry_run: bool = False
+    verbose: bool
+    dry_run: bool
 
 
 @cli.callback()
 def main(
     ctx: typer.Context,
-    trilium_url: Annotated[str, typer.Option("--service-url", envvar="TRILIUM_URL", is_eager=True)],
-    token: Annotated[str, typer.Option("--token", envvar="TRILIUM_TOKEN", is_eager=True)],
-    verbose: Annotated[
-        bool,
-        typer.Option(
-            "--verbose",
-            "-v",
-            is_flag=True,
-            is_eager=True,
-            help="Display additional columns, defaults to False",
-        ),
-    ] = False,
-    version: Annotated[
-        bool,
-        typer.Option(
-            "--version",
-            "-V",
-            callback=version_callback,
-            is_flag=True,
-            is_eager=True,
-            help="Show version and exit",
-        ),
-    ] = False,
     dry_run: Annotated[
         bool,
         typer.Option(
             "--dry-run",
             "-n",
-            is_flag=True,
-            is_eager=True,
-            help="Render Tasks as table rather than updating Trilium, defaults to False",
+            help="Render Tasks to console rather than updating Trilium.",
         ),
     ] = False,
+    verbose: Annotated[
+        bool,
+        typer.Option(
+            "--verbose",
+            "-V",
+            help="Display additional columns, defaults to False.",
+        ),
+    ] = False,
+    version: Annotated[
+        Optional[bool],
+        typer.Option(
+            "--version",
+            "-v",
+            callback=version_callback,
+            is_eager=True,
+            help="Show version and exit.",
+        ),
+    ] = None,
 ) -> None:
-    """Initialize application.
+    """Curate Tasks for TaskManager in trilium notes.
 
-    :param ctx: typer context
-    :param trilium_url: URL for Trilium service
-    :param token: Trilium API token
-    :param verbose: display additional columns or data
-    :param version: show version and exit
-    :param dry_run: render table rather than updating Trilium
-    :raises typer.Abort: if taskTodoRoot not found
+    * **#taskTodoRoot** is the root of Due Tasks.
+
+    * **#taskDoneRoot** is the root for Done Tasks.
     """
-    client = Session(trilium_url, token)
-    ctx.with_resource(client)
-
-    todo_root = client.search("#taskTodoRoot")
-    if len(todo_root) != 1:
-        typer.echo("taskTodoRoot not found", err=True)
-        raise typer.Abort()
-
-    ctx.obj = State(trilium=client, root=todo_root[0], verbose=verbose, dry_run=dry_run)
+    ctx.obj = State(verbose=verbose, dry_run=dry_run)
 
 
-@cli.command(name="update", help="Update task in TaskManager")
+@cli.command(name="update", help="Update Task.")
 @cli.command()
 def add(
     ctx: typer.Context,
     description: Description,
     due: Annotated[
         Optional[datetime],
-        typer.Option("--due", formats=["%Y-%m-%d"], help="Date Task is due, defaults to None"),
+        typer.Option("--due", formats=["%Y-%m-%d"], help="Date Task is due, defaults to None."),
     ] = None,
     location: Annotated[
-        Optional[str], typer.Option("--location", help="Location field for Task, defaults to None")
+        Optional[str], typer.Option("--location", help="Location field for Task, defaults to None.")
     ] = None,
     tags: Annotated[
         Optional[list[str]],
         typer.Option(
-            "--tag", "-t", help="Tag field(s) for Task, repeat as needed, defaults to None"
+            "--tag", "-t", help="Tag field(s) for Task, repeat as needed, defaults to None."
         ),
     ] = None,
-    content: Annotated[
+    message: Annotated[
         Optional[str],
-        typer.Option("--message", "-m", help="Optional content for Task, defaults to None"),
+        typer.Option("--message", "-m", help="Optional content for Task, defaults to None."),
     ] = None,
+    content: Annotated[Optional[typer.FileText], typer.Option()] = None,
 ) -> None:
-    """Add task to TaskManager.
+    """Add Task to TaskManager."""
 
-    :param ctx: typer context
-    :param description: description of task
-    :param due: due date for task
-    :param location: location for task
-    :param tags: tag(s) for task
-    :param content: additional content for task
-    """
-    session = ctx.obj.trilium
-    todo_root = ctx.obj.root
+    if message and content:
+        raise typer.BadParameter("Cannot specify both --message and --content")
 
+    session: Session = open_session(ctx)
     task_template = session.search('#task note.title="task template"')[0]
+    todo_root = session.search("#taskTodoRoot")[0]
 
     if ctx.obj.dry_run:
         typer.echo(
-            f"{ctx.command.name} {description} (todoDate={due}, tags={', '.join(tags or [])}, location={location})"
+            f"{ctx.command.name} {description} (todoDate={due},"
+            f" tags={None if not tags else ', '.join(tags)}, location={location})"
         )
-        return
+        raise typer.Exit()
 
     task: Note
     match ctx.command.name:
@@ -184,45 +173,43 @@ def add(
     if location:
         task["location"] = location
 
-    if content:
-        task.content = content
+    if message:
+        task.content = message
 
-    task += [Label("tag", t) for t in tags or []]
+    if content:
+        task.content = content.read()
+
+    if tags and len(tags) > 0:
+        task += [Label("tag", t) for t in tags]
 
     todo_root += task
 
 
+@cli.command(name="ls")
 @cli.command()
 def list(ctx: typer.Context) -> None:
-    """List due tasks from TaskManager
+    """List due Tasks."""
 
-    :param ctx: typer context
-    """
-    table = Table("Due", "Description", box=None, header_style="underline2")
+    table = Table("Due", "Description", title="Tasks", box=None, header_style="underline2")
     if ctx.obj.verbose:
         table.add_column("Location")
         table.add_column("Tag(s)")
 
-    todo_root: Note = ctx.obj.root
+    session: Session = open_session(ctx)
+    todo_root = session.search("#taskTodoRoot")[0]
     for task in todo_root.children:
         row = []
 
-        try:
-            row.append(task["todoDate"])
-        except KeyError:
-            row.append("")
+        row.append("N/A" if "todoDate" not in task else task["todoDate"])
         row.append(task.title)
 
         if ctx.obj.verbose:
-            try:
-                row.append(task["location"])
-            except KeyError:
-                row.append("")
+            row.append("N/A" if "location" not in task else task["location"])
 
-            try:
-                row.append(", ".join([t.value for t in task.attributes["tag"]]))
-            except KeyError:
-                row.append("")
+            tags: str = "N/A"
+            if "tag" in task.attributes and len(task.attributes["tag"]) > 0:
+                tags = ", ".join([t.value for t in task.attributes["tag"]])
+            row.append(tags)
 
         table.add_row(*row)
 
@@ -231,34 +218,28 @@ def list(ctx: typer.Context) -> None:
             console.print(table)
 
 
+@cli.command("rm")
 @cli.command()
-def rm(
+def delete(
     ctx: typer.Context,
     description: Description,
     confirm: bool = typer.Option(
-        False, "--confirm", is_flag=True, help="Confirm delete, defaults to False"
+        False, "--confirm", is_flag=True, help="Confirm delete, defaults to False."
     ),
 ) -> None:
-    """Delete task from TaskManager.
+    """Delete Task."""
 
-    :param ctx: typer context
-    :param description: description of task
-    :param confirm: confirm delete. Defaults to False.
-    :raises typer.Abort: if failed to delete task not found
-    """
-    task = ctx.obj.trilium.search(f'#task note.title="{description}"', include_archived_notes=True)
-    if len(task) == 0:
-        typer.echo(f"Task '{description}'not found", err=True)
-        raise typer.Abort()
+    session: Session = open_session(ctx)
+    task = session.search(f'#task note.title="{description}"', include_archived_notes=True)[0]
 
     if ctx.obj.dry_run:
         typer.echo(f"{ctx.command.name} {description} (confirm={confirm})")
-        return
+        raise typer.Exit()
 
     if not confirm:
         typer.confirm(f"Delete {description}?", default=False, show_default=True, abort=True)
 
-    task[0].delete()
+    task.delete()
 
 
 @cli.command()
@@ -268,50 +249,38 @@ def done(
     done: Annotated[
         Optional[datetime],
         typer.Option(
-            "--completed", formats=["%Y-%m-%d"], help="Date task was completed, defaults to today"
+            "--completed", formats=["%Y-%m-%d"], help="Date task was completed, defaults to today."
         ),
     ] = None,
 ) -> None:
-    """Mark task as done in TaskManager.
+    """Mark Task as done."""
 
-    Note: All the heavy lifting is done by the JavaScript Implementation of TaskManager.
+    now = (done or datetime.now()).strftime("%Y-%m-%d")
 
-    :param ctx: typer context
-    :param description: description of task
-    :raises typer.Abort: if taskDoneRoot not found
-    """
-    task = ctx.obj.trilium.search(f'#task note.title="{description}"', ancestor_note=ctx.obj.root)
-    if len(task) == 0:
-        typer.echo(f"Task '{description}'not found", err=True)
-        raise typer.Abort()
-
-    now = datetime.utcnow().strftime("%Y-%m-%d") if done is None else done.strftime("%Y-%m-%d")
+    # Note: All the heavy lifting is done by the JavaScript Implementation of TaskManager.
+    session: Session = open_session(ctx)
+    task = session.search(f'#task note.title="{description}"')[0]
 
     if ctx.obj.dry_run:
         typer.echo(f"#doneDate={now} #cssClass=done {description}")
-        return
+        raise typer.Exit()
 
-    task[0]["doneDate"] = now
-    task[0]["cssClass"] = "done"
+    task["doneDate"] = now
+    task["cssClass"] = "done"
 
 
 @cli.command()
 def archive(ctx: typer.Context, description: Description) -> None:
-    """Archive TaskManager Task in Trilium.
+    """Archive Task."""
 
-    :param ctx: typer context
-    :param description: description of task
-    """
-    task = ctx.obj.trilium.search(f'#task note.title="{description}"')
-    if len(task) == 0:
-        typer.echo(f"Task '{description}'not found", err=True)
-        raise typer.Abort()
+    session: Session = open_session(ctx)
+    task = session.search(f'#task note.title="{description}"')[0]
 
     if ctx.obj.dry_run:
         typer.echo(f"{ctx.command.name} {description}")
-        return
+        raise typer.Exit()
 
-    task[0]["archived"] = ""
+    task["archived"] = ""
 
 
 if __name__ == "__main__":
